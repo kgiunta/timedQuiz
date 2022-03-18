c2473625998db9722c3c08405e43befb2577d033
Kgiunta <giunta.kyle@yahoo.com>
2022-03-17 21:40:28 -0400

    puesdocoding

---
+++ script.js
@@ -0,0 +1,42 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+<button onclick="functionToExecute()">Click</button>
+
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+
+// What country was not part of the original Axis powers in World War II?
+
+// Germany 
+// Italy 
+// Russia*
+// Japan
+
+// What is "cynophobia"?
+// Fear of Drugs
+// Fear of Dogs*
+// Fear of Cats 
+// Fear of Cyclones
+
+// What is the rarest M&M color?
+
+// Brown*
+// Green
+// Blue 
+// Red
+
+// Which is the only edible food that never goes bad?
+
+// Twnikies
+// Honey*
+// Velveta Cheese
+// Marshmellow
+
+
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
